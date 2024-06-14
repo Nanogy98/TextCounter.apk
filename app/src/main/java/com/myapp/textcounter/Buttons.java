8528b55def07198947bec9cb8cb951dd3d57440b
Nanogy98 <97442619+Nanogy98@users.noreply.github.com>
2024-06-15 02:19:57 +0900

    Counter_v1.4

--- app/src/main/java/com/myapp/textcounter/Buttons.java
+++ app/src/main/java/com/myapp/textcounter/Buttons.java
@@ -2,33 +2,37 @@ package com.myapp.textcounter;
 
 import android.os.Build;
 import android.text.Editable;
-public class Buttons  {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Buttons extends AppCompatActivity {
     //テキストのカウント
     //edittextの文字列の長さを取得するメソッド
-    public String Count(Editable text){
-        return Integer.toString (text.length());
+    public String Count(Editable text) {
+        return Integer.toString (text.length ( ));
     }
+
     //edittextの文字列の長さが0であるか判断するメソッド
-    public boolean Judge(Editable s){
-        int L = s.toString ().length();
-        if(L == 0){
+    public boolean Judge(Editable s) {
+        int L = s.toString ( ).length ( );
+        if (L == 0) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
     //Int型からstring型に変換するメソッド
-    public String format(int lines){
+    public String format(int lines) {
         return Integer.toString (lines);
     }
+
     //OS Version Check
-    public boolean ChkOS(int ver){
-        if(Build.VERSION.SDK_INT<=ver ){
+    public boolean ChkOS(int ver) {
+        if (Build.VERSION.SDK_INT <= ver) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 }
+
--- app/src/main/java/com/myapp/textcounter/MainActivity.java
+++ app/src/main/java/com/myapp/textcounter/MainActivity.java
@@ -4,33 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ClipboardManager;
 import android.content.ClipData;
 import android.content.Context;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.text.TextWatcher;
+import android.text.Editable;
 import android.view.View;
 import android.os.Bundle;
-import android.text.Editable;
 import java.util.Objects;
 import static androidx.appcompat.app.AppCompatDelegate.*;
 
 public class MainActivity extends AppCompatActivity {//継承
-    //フィールド
-    private TextView textLetter,textLines;
-    private EditText editText;
-    Buttons bt = new Buttons();
+
+    //フィールドonCreate内で呼び出すために、privateで宣言
+    private Buttons bt ;
+    private EditText e;
+    private TextView textLetter ,textLines;
+    private ClipboardManager clipboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //使用アイテムの定義
-        editText = findViewById(R.id.edit_text);
-        textLetter  = findViewById(R.id.text_letters2);
-        textLines = findViewById(R.id.text_lines2);
+        //Activity起動時リソースのidに呼び出して、フィールドに代入
+        bt = new Buttons();
+        e = findViewById(R.id.edit_text);
+        textLetter = findViewById (R.id.text_letters2);
+        textLines = findViewById (R.id.text_lines2);
+        clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
 
         //テキストカウント
         Button button_cnt = findViewById(R.id.button_cnt);
@@ -45,23 +51,17 @@ public class MainActivity extends AppCompatActivity {//継承
         Button button_pst = findViewById(R.id.button_pst);
         button_pst.setOnClickListener(new ButtonPaste());
 
-        //スイッチの イベントを作成
+        //チェックボックス(TextWachar)
+        CheckBox CB = findViewById(R.id.checkBox);
+        CB.setOnCheckedChangeListener (new Check());
+
+        //スイッチ(テーマ変更) Activity開始時にクラスを呼ぶ
         //Switch toggleSwitch = findViewById(R.id.TG_switch);
         ToggleButton toggleSwitch = findViewById(R.id.TG_switch);
-        toggleSwitch.setOnCheckedChangeListener(new onCheckedChangeListener());
-
-        //起動時にシステムのダークモードがYESの場合はtrue(ダークモードオン)
-        if(getDefaultNightMode() == MODE_NIGHT_YES||getDefaultNightMode()==MODE_NIGHT_AUTO_BATTERY){
-            toggleSwitch.setChecked(true);
-        }
-        //そうでない場合はfalse(無効)
-        else if(getDefaultNightMode()== MODE_NIGHT_NO){
-            toggleSwitch.setChecked(false);
-        }
+        toggleSwitch.setOnCheckedChangeListener(new Theme());
     }
 
-    // トグルスイッチを押した時に処理するクラス。
-    class onCheckedChangeListener implements CompoundButton.OnCheckedChangeListener{
+    static class Theme implements CompoundButton.OnCheckedChangeListener{
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
             if (isChecked) {
                 setDefaultNightMode(MODE_NIGHT_YES);
@@ -71,16 +71,38 @@ public class MainActivity extends AppCompatActivity {//継承
             }
         }
     }
-     class ButtonCount implements View.OnClickListener {
-        EditText e = editText;
+
+    /*
+    TextEvent型のTWにTextEventクラスのobj生成させることで、
+    TWでaddイベントを呼ぶと、removeする際に同じ場所(TW)を参照することでwatcherが切れる
+    */
+    class Check implements CompoundButton.OnCheckedChangeListener{
+        TextEvent TW = new TextEvent ();
+        public void onCheckedChanged(CompoundButton buttonView1, boolean isChecked) {
+            View (bt.Count (e.getText ( )), bt.format (e.getLineCount ( )));
+            if (isChecked) {
+                e.addTextChangedListener (TW);
+            }else{
+                e.removeTextChangedListener (TW);
+            }
+        }
+    }
+    class TextEvent implements TextWatcher {
+        public void beforeTextChanged(CharSequence charSequence, int A, int B, int C) {}
+        public void onTextChanged(CharSequence charSequence, int A, int B, int C) {}
+        public void afterTextChanged(Editable editable) {
+            View (bt.Count (e.getText ( )), bt.format (e.getLineCount ( )));
+        }
+    }
+
+    class ButtonCount implements View.OnClickListener {
         public void onClick(View view) {
-            if(bt.Judge(e.getText())){toast();}
-            View(bt.Count(e.getText()),bt.format(e.getLineCount()));
+            if (bt.Judge (e.getText ( ))) {toast ( R.string.noText);}
+            View (bt.Count (e.getText ( )), bt.format (e.getLineCount ( )));
         }
     }
 
     class ButtonDelete implements View.OnClickListener {
-        EditText e = editText;
         public void onClick(View view){
             e.getText().clear();
             View(bt.Count(e.getText()),bt.format(e.getLineCount()));
@@ -88,30 +110,20 @@ public class MainActivity extends AppCompatActivity {//継承
     }
 
     class ButtonCopy implements View.OnClickListener {
-        //クリップボードマネージャー
-        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        Editable e = editText.getText();
         public void onClick(View view) {
-            if(bt.Judge(e)){toast();}
+            if(bt.Judge(e.getText ())){toast(R.string.noText);}
             else{
-                // Editのテキストを取得
-                ClipData clip = ClipData.newPlainText(null, e);
-                //クリップボードにセット
-                clipboard.setPrimaryClip(clip);
+                ClipData clip = ClipData.newPlainText(null, e.getText ());
+                clipboard.setPrimaryClip(clip);//クリップボードにセット
             }
             //OS 12L以下かつ、edittext内の文字の長さが0でない時だけバブルを出す
-            if(bt.ChkOS(32) && e.length() !=0) {
-                Toast.makeText(getApplicationContext(), R.string.copy, Toast.LENGTH_SHORT).show();
-            }
+            if(bt.ChkOS(32) && e.length() !=0) {toast(R.string.copy);}
         }
     }
 
     class ButtonPaste implements View.OnClickListener {
-        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        EditText e = editText;
         public void onClick(View view) {
-            try {
-                //グリップボートのデータの位置     0番目
+            try {//グリップボートのデータの位置  0番目
                 ClipData.Item item = Objects.requireNonNull (clipboard.getPrimaryClip ( )).getItemAt (0);
                 String pasteData = item.getText().toString();
                 e.setText (pasteData);
@@ -120,9 +132,6 @@ public class MainActivity extends AppCompatActivity {//継承
             catch (Exception e) {System.out.println(e); return;}
         }
     }
-    void toast(){Toast.makeText(getApplicationContext(), R.string.noText, Toast.LENGTH_SHORT).show();}
-    void View(String a,String b){
-        textLetter.setText (a);
-        textLines.setText(b);
-    }
+    void toast(int id){Toast.makeText(getApplicationContext(), id, Toast.LENGTH_SHORT).show();}
+    void View(String a,String b){textLetter.setText (a);textLines.setText(b);}
 }
