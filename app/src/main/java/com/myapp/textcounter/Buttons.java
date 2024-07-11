68d1653ea606e669ffdabaa43409c823e38ac56d
LaIixia <97442619+LaIixia@users.noreply.github.com>
2024-07-11 18:38:01 +0900

    Counter_v1.5

--- app/src/main/java/com/myapp/textcounter/Buttons.java
+++ app/src/main/java/com/myapp/textcounter/Buttons.java
@@ -2,37 +2,40 @@ package com.myapp.textcounter;
 
 import android.os.Build;
 import android.text.Editable;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.EditText;
 
-public class Buttons extends AppCompatActivity {
-    //テキストのカウント
+public class Buttons  {
+
+    //edittextのテキストの行数を取得するメソッド
+    public String line(EditText text) {return format(text.getLineCount());}
     //edittextの文字列の長さを取得するメソッド
-    public String Count(Editable text) {
-        return Integer.toString (text.length ( ));
-    }
+    public String Count(Editable text) {return format(text.length ( ));}
+    //SubActivity用
+    public String Count(String text) {return format(text.length ( ));}
+    //edittextの改行抜きでテキストの文字数を取得するメソッド
+    public String BreakCnt(String letters,String lines) {return format((Integer.parseInt(letters) - Integer.parseInt(lines))+1);}
+    //空白抜きでテキストの文字数をカウント
+    public String EmptyCnt(String letters) {return format(letters.replaceAll("　", "").length());}
+    //Intをstring型に変換するメソッド
+    public String format(int lines) {return String.valueOf (lines);}
 
     //edittextの文字列の長さが0であるか判断するメソッド
-    public boolean Judge(Editable s) {
-        int L = s.toString ( ).length ( );
-        if (L == 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    //Int型からstring型に変換するメソッド
-    public String format(int lines) {
-        return Integer.toString (lines);
-    }
-
+    public boolean Judge(Editable l) {return Integer.parseInt (Count (l)) == 0;}//0の場合だけtrueを返す
     //OS Version Check
-    public boolean ChkOS(int ver) {
-        if (Build.VERSION.SDK_INT <= ver) {
+    public boolean ChkOS(int v) {return Build.VERSION.SDK_INT <= v;}//osが引数v以下の場合だけtrueを返す
+
+   /*public boolean Judge(Editable l) {
+        if (Integer.parseInt (Count (l)) == 0) {
             return true;
         } else {
             return false;
         }
     }
+    public boolean ChkOS(int v) {
+        if (Build.VERSION.SDK_INT <= v) {
+            return true;
+        } else {
+            return false;
+        }
+    }*/
 }
-
--- app/src/main/java/com/myapp/textcounter/MainActivity.java
+++ app/src/main/java/com/myapp/textcounter/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ClipboardManager;
 import android.content.ClipData;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
@@ -20,16 +21,23 @@ import static androidx.appcompat.app.AppCompatDelegate.*;
 
 public class MainActivity extends AppCompatActivity {//継承
 
-    //フィールドonCreate内で呼び出すために、privateで宣言
-    private Buttons bt ;
-    private EditText e;
-    private TextView textLetter ,textLines;
-    private ClipboardManager clipboard;
+    //フィールド onCreate内で呼び出すために、ここで各クラスを宣言
+    Buttons bt ;
+    EditText e;
+    TextView textLetter ,textLines;
+    ClipboardManager clipboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       /* int dpi = getResources().getDisplayMetrics().densityDpi;
+        if(dpi <=420){
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }*/
 
         //Activity起動時リソースのidに呼び出して、フィールドに代入
         bt = new Buttons();
@@ -37,10 +45,13 @@ public class MainActivity extends AppCompatActivity {//継承
         textLetter = findViewById (R.id.text_letters2);
         textLines = findViewById (R.id.text_lines2);
         clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        System.out.println(e.getLineCount());
+        View (bt.Count (e.getText ( )), bt.format (e.getLineCount ()));
 
         //テキストカウント
         Button button_cnt = findViewById(R.id.button_cnt);
         button_cnt.setOnClickListener(new ButtonCount());
+        button_cnt.setOnLongClickListener(new Buttonl());
         //テキスト削除
         Button button_del = findViewById(R.id.button_del);
         button_del.setOnClickListener(new ButtonDelete());
@@ -61,7 +72,18 @@ public class MainActivity extends AppCompatActivity {//継承
         toggleSwitch.setOnCheckedChangeListener(new Theme());
     }
 
-    static class Theme implements CompoundButton.OnCheckedChangeListener{
+    class Buttonl implements View.OnLongClickListener {
+        public boolean onLongClick(View v) {
+            Intent MS = new Intent(MainActivity.this, SubActivity.class)
+                    .putExtra("text data",String.valueOf (e.getText ()))//テキストの内容
+                    .putExtra("text lines",bt.line (e));//行
+            startActivity(MS);//subActivityの開始
+            //finish();ActivityのEdittextを保持させておくため
+            return false;
+        }
+    }
+
+     class Theme implements CompoundButton.OnCheckedChangeListener{
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
             if (isChecked) {
                 setDefaultNightMode(MODE_NIGHT_YES);
@@ -77,9 +99,9 @@ public class MainActivity extends AppCompatActivity {//継承
     TWでaddイベントを呼ぶと、removeする際に同じ場所(TW)を参照することでwatcherが切れる
     */
     class Check implements CompoundButton.OnCheckedChangeListener{
-        TextEvent TW = new TextEvent ();
         public void onCheckedChanged(CompoundButton buttonView1, boolean isChecked) {
-            View (bt.Count (e.getText ( )), bt.format (e.getLineCount ( )));
+            TextEvent TW = new TextEvent ();
+            View (bt.Count (e.getText ( )), bt.line(e));
             if (isChecked) {
                 e.addTextChangedListener (TW);
             }else{
@@ -91,21 +113,21 @@ public class MainActivity extends AppCompatActivity {//継承
         public void beforeTextChanged(CharSequence charSequence, int A, int B, int C) {}
         public void onTextChanged(CharSequence charSequence, int A, int B, int C) {}
         public void afterTextChanged(Editable editable) {
-            View (bt.Count (e.getText ( )), bt.format (e.getLineCount ( )));
+            View (bt.Count (e.getText ( )), bt.line(e));
         }
     }
 
     class ButtonCount implements View.OnClickListener {
         public void onClick(View view) {
-            if (bt.Judge (e.getText ( ))) {toast ( R.string.noText);}
-            View (bt.Count (e.getText ( )), bt.format (e.getLineCount ( )));
+            if (bt.Judge(e.getText ( ))) {toast ( R.string.noText);}
+            View(bt.Count(e.getText()),bt.line(e));
         }
     }
 
     class ButtonDelete implements View.OnClickListener {
         public void onClick(View view){
             e.getText().clear();
-            View(bt.Count(e.getText()),bt.format(e.getLineCount()));
+            View(bt.Count(e.getText()),bt.line(e));
         }
     }
 
@@ -117,7 +139,7 @@ public class MainActivity extends AppCompatActivity {//継承
                 clipboard.setPrimaryClip(clip);//クリップボードにセット
             }
             //OS 12L以下かつ、edittext内の文字の長さが0でない時だけバブルを出す
-            if(bt.ChkOS(32) && e.length() !=0) {toast(R.string.copy);}
+            if(bt.ChkOS(32) && !bt.Judge (e.getText ())) {toast(R.string.copy);}
         }
     }
 
@@ -127,11 +149,11 @@ public class MainActivity extends AppCompatActivity {//継承
                 ClipData.Item item = Objects.requireNonNull (clipboard.getPrimaryClip ( )).getItemAt (0);
                 String pasteData = item.getText().toString();
                 e.setText (pasteData);
-                View(bt.format(pasteData.length()),bt.format(e.getLineCount()));
+                View(bt.format(pasteData.length()),bt.line(e));
             }
             catch (Exception e) {System.out.println(e); return;}
         }
     }
-    void toast(int id){Toast.makeText(getApplicationContext(), id, Toast.LENGTH_SHORT).show();}
+    void toast(int resId){Toast.makeText(getApplicationContext(), resId, Toast.LENGTH_SHORT).show();}
     void View(String a,String b){textLetter.setText (a);textLines.setText(b);}
 }
